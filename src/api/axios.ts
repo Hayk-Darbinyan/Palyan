ddecab8bbd39087f7cc2f74a051bd2dd5be4f401
Hayk-Darbinyan <haykdarbinyan04@gmail.com>
2026-01-08 14:09:21 +0400

    refactor: bug fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,3 +5,17 @@ const apiUrl = import.meta.env.VITE_APP_API_URL;
 export const api = axios.create({
   baseURL: apiUrl,
 });
+
+api.interceptors.request.use((config) => {
+  if (!config.url) return config;
+
+  if (config.url.startsWith("/admin")) {
+    const token = localStorage.getItem("adminToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
+});
